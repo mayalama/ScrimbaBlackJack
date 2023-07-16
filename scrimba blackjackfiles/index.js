5f063b8a2065281b0cf67ea4c5144d6ce914c97d
mayalama <74232122+mayalama@users.noreply.github.com>
2023-07-16 12:30:50 +0200

    Add files via upload

---
+++ scrimba blackjackfiles/index.js
@@ -0,0 +1,27 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+// 1. Store the message-el paragraph in a variable called messageEl
+ let messageEl= document.getElementById('message-el');
+ console.log("messageEl")
+
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+function startGame(){
+
+  if (sum <= 20) {
+      message = "Do you want to draw a new card? "
+  } else if (sum === 21) {
+      message = "Wohoo! You've got Blackjack! "
+      hasBlackJack = true
+  } else {
+      message = "You're out of the game! "
+      isAlive = false
+  }
+// 2. Display the message in the messageEl using messageEl.textContent
+  messageEl.textContent=message;
+}
